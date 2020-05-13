6a69071cd1064a3e98d6ba8aad3a2c5d28627edc
Alberto Negron <albertonegron@gmail.com>
2020-05-13 11:12:04 +0100

    remove explicit conditional

--- ruby/two-fer/two_fer.rb
+++ ruby/two-fer/two_fer.rb
@@ -3,8 +3,6 @@
 # TwoFer class
 class TwoFer
   def self.two_fer(name = nil)
-    return 'One for you, one for me.' if name.nil?
-
-    "One for #{name}, one for me."
+    "One for #{(name ||= '').empty? ? 'you' : name}, one for me."
   end
 end
