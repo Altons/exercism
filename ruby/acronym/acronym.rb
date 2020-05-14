5a3cfb829a71892c896c4873ddef1c1bff494b27
Alberto Negron <albertonegron@gmail.com>
2020-05-14 19:24:51 +0100

    remove comments

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -1,10 +1,5 @@
 # frozen_string_literal: true
 
-# Write your code for the 'Acronym' exercise in this file. Make the tests in
-# `acronym_test.rb` pass.
-#
-# To get started with TDD, see the `README.md` file in your
-# `ruby/acronym` directory.
 class Acronym
   def self.abbreviate(str)
     str.sub(/-/, ' ').split.map { |c| c[0].upcase }.join
