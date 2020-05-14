8ce7669645f6d391421b8828910edf4d33141281
Alberto Negron <albertonegron@gmail.com>
2020-05-14 17:49:19 +0100

    [ruby resistor] 2nd iter

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 class ResistorColorDuo
-  COLORS = { 'black' => '0', 'brown' => '1', 'red' => '0', 'orange' => '3',
-             'yellow' => '4', 'green' => '5', 'blue' => '6', 'violet' => '7',
-             'grey' => '8', 'white' => '9' }.freeze
+  COLORS = %w[black brown red orange yellow green blue violet
+              grey white].freeze
   def self.value(lst)
-    lst.map { |v| COLORS[v] }[0..1].join.to_i
+    lst.first(2).map { |v| COLORS.index(v) }.join.to_i
   end
 end
