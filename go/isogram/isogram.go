748492d786ccefc90bf6490bcb6f99c6a4bc0a3c
Alberto Negron <albertonegron@gmail.com>
2020-05-15 14:37:31 +0100

    [go isogram] move nested condition

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -9,10 +9,11 @@ func IsIsogram(s string) bool {
 	lst := strings.Split(strings.ToLower(s), "")
 	repeated := make(map[string]bool)
 	for _, c := range lst {
+		if c == "-" || c == " " {
+			continue
+		}
 		if repeated[c] {
-			if c == "-" || c == " " {
-				continue
-			}
+
 			return false
 		}
 		repeated[c] = true
