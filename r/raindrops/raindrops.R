88979283b3a1d069420b1555d3a23186b5b8d792
Alberto Negron <albertonegron@gmail.com>
2020-05-26 14:55:56 +0100

    [r raindrops] vectorized solution 2

--- r/raindrops/raindrops.R
+++ r/raindrops/raindrops.R
@@ -1,12 +1,7 @@
 raindrops <- function(number) {
-  r <- ""
   divisors <- c(3, 5, 7)
   sounds <- c("Pling", "Plang", "Plong")
-  for (i in 1:length(divisors)) {
-    if (number %% divisors[i] == 0) {
-      r <- paste0(r, sounds[i])
-    }
-  }
+  r <- paste(sounds[number %% divisors ==0],collapse="")
   if (r == "") {
     return(paste0(number))
   }
