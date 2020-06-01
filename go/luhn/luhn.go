e4843994dd6c479d7405b607d73277396702c129
Alberto Negron <albertonegron@gmail.com>
2020-06-01 17:35:54 +0100

    [go luhn] final solution

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -3,36 +3,29 @@ package luhn
 import (
 	"strconv"
 	"strings"
-	"unicode"
 )
 
 //Valid check whether or not it is valid per the Luhn formula
 func Valid(s string) bool {
 	newS := strings.ReplaceAll(s, " ", "")
-
 	if len(newS) < 2 {
 		return false
 	}
-	nDigits := len(newS)
-	// parity := nDigits % 2
-	sum := 0
-	double := false
-	for i := nDigits - 1; i >= 0; i-- {
-		char := newS[i]
-		if unicode.IsDigit(rune(char)) == false {
+	double := len(newS)%2 == 0
+	var sum int
+	for _, d := range newS {
+		digit, err := strconv.Atoi(string(d))
+		if err != nil {
 			return false
 		}
-		digit, _ := strconv.Atoi(string(char))
 		if double {
 			digit *= 2
 			if digit > 9 {
 				digit -= 9
 			}
-
 		}
 		sum += digit
 		double = !double
-
 	}
 	return sum%10 == 0
 }
