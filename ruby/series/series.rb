74ee5ab4eeba8b8e6374f06f519199882baca6eb
Alberto Negron <albertonegron@gmail.com>
2020-06-06 10:48:27 +0100

    [ruby series] revised sol

--- ruby/series/series.rb
+++ ruby/series/series.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Series
+  attr_reader :digits
   def initialize(digits)
     @digits = digits
   end
@@ -8,6 +9,8 @@ class Series
   def slices(n)
     return raise ArgumentError if @digits.size < n
 
-    @digits.size.times.each_with_object([]) { |i, a| a << @digits[i, n] if i + n <= @digits.size }
+    a = []
+    digits.chars.each_cons(n) { |e| a << e.join }
+    a
   end
 end
