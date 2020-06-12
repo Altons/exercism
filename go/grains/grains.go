f6d362d1fce1ff0bcb53560ea7876f92818c78eb
Alberto Negron <albertonegron@gmail.com>
2020-06-12 17:06:34 +0100

    [go grains] add custom Pow func

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -2,7 +2,6 @@ package grains
 
 import (
 	"fmt"
-	"math"
 )
 
 //Square ...
@@ -10,14 +9,28 @@ func Square(number int) (uint64, error) {
 	if number <= 0 || number > 64 {
 		return uint64(1), fmt.Errorf("Choose a positive number")
 	}
-	return uint64(math.Pow(2, float64(number)-1)), nil
+	return Pow(2, uint64(number)-1), nil
 }
 
 //Total ....
 func Total() uint64 {
 	var total uint64
 	for i := 1; i <= 64; i++ {
-		total += uint64(math.Pow(2, float64(i)-1))
+		total += Pow(2, uint64(i)-1)
 	}
 	return total
+}
+
+//Pow calculates compute x^n using binary powering algorithm
+func Pow(a, b uint64) uint64 {
+	var p uint64
+	p = 1
+	for b > 0 {
+		if b&1 != 0 {
+			p *= a
+		}
+		b >>= 1
+		a *= a
+	}
+	return p
 }
