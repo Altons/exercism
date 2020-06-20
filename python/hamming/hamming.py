1e466e3449a0b4f6bbb438f37c2b8ada2dce8f3a
Alberto Negron <albertonegron@gmail.com>
2020-06-20 11:30:02 +0100

    [python hamming] update solution

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,4 +1,4 @@
 def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError("Error: Strands are not equal in size")
-    return sum(list(map(lambda x: x[0] != x[1], zip([c for c in strand_a], [c for c in strand_b]))))
+    return sum([1 for x in zip(strand_a, strand_b) if x[0] != x[1]])
